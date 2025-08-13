e6cf573e5fbd896cccb2b194c7090d1c41f47c2d
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-08-13 10:19:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-
 function firstWord(s) {
-  // your code here
-	
- let word = s.trim();
- if(s.length === "") return;
-	return s.split(/\s+/)[0];
+  let word = s.trim(); // remove extra spaces
+  if (word.length === 0) return ""; // empty string case
+  return word.split(/\s+/)[0]; // split by spaces and return first word
 }
 
 const s = prompt("Enter String:");
